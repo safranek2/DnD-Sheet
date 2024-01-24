0ad5bc3769670f278a85b3cb00592d9474f3b2e4
safranek2 <safranek2@spsejecna.cz>
2024-01-24 16:53:34 +0100

    Add Favicon with Theme-aware SVG Styling
    Introduced a favicon (`logo.svg`) to the project. The `_Layout.cshtml` file has been updated to include the new favicon link. Additionally, the SVG styling has been adjusted with a class (`a`) that adapts its fill color based on the user's preferred color scheme. In light mode, the fill color is `#1d1d1b` (black), while in dark mode, it switches to `#ffffff` (white). This ensures a seamless visual experience for users in different color schemes.

--- DnD Sheet/DnD Sheet/obj/Debug/net7.0/DnD Sheet.AssemblyInfo.cs
+++ DnD Sheet/DnD Sheet/obj/Debug/net7.0/DnD Sheet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnD Sheet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff1c058b8c694f3cdba81953db8a88db3c74d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnD Sheet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnD Sheet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
